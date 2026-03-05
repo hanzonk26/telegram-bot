d93c2c77a5bcd57956737e2fdc97176eb5ef239d
hanzonk26 <hanzonk26@gmail.com>
2026-03-05 11:36:29 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,12 +11,20 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def btc(update: Update, context: ContextTypes.DEFAULT_TYPE):
     try:
         url = "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
-        data = requests.get(url).json()
+
+        headers = {
+            "User-Agent": "Mozilla/5.0"
+        }
+
+        response = requests.get(url, headers=headers, timeout=10)
+        data = response.json()
+
         price = data["price"]
 
         await update.message.reply_text(f"Harga BTC sekarang: ${price}")
 
     except Exception as e:
+        print(e)
         await update.message.reply_text("API harga BTC error")
 
 app = ApplicationBuilder().token(TOKEN).build()
