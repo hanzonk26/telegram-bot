22d6155de38c2ddc9ef56e18cb3af6956b1f67cb
hanzonk26 <hanzonk26@gmail.com>
2026-03-05 13:34:46 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,10 +10,10 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 async def btc(update: Update, context: ContextTypes.DEFAULT_TYPE):
     try:
-        url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
+        import requests
 
-        response = await context.bot._request.get(url)
-        data = response.json()
+        url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
+        data = requests.get(url).json()
 
         price = data["bitcoin"]["usd"]
 
